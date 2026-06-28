7309e356e352cd227bf0a652f620cff75b90628b
Stan <taix97@outlook.com>
2026-06-28 20:15:04 +0300

    player sync

--- Assets/Scripts/Steamworks.NET/CustomNetworkManager.cs
+++ Assets/Scripts/Steamworks.NET/CustomNetworkManager.cs
@@ -1,12 +1,21 @@
 using Mirror;
 using Mirror.FizzySteam;
+using UnityEngine;
 
 public class CustomNetworkManager : NetworkManager
 {
+    public Transform startPoint;
+
     public override void Awake()
     {
-        if(transport == null)
+        if (transport == null)
             transport = GetComponent<FizzySteamworks>();
         base.Awake();
+    }
+
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        Instantiate(playerPrefab, startPoint.position, Quaternion.identity);
+        NetworkServer.AddPlayerForConnection(conn, playerPrefab);
     }
 }
